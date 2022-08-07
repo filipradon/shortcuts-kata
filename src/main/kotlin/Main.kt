f124701e6e41c37ba6b4af21cb0ec78edd759aae
Filip Radoń <filip.radon@ocado.com>
2022-08-07 17:21:48 +0200

    First shortcut ⌘ + D

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*
+    Hiya!👋 Start here!
+     */
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    /*
+    First shortcut! - ⌘ D - Duplicate line
+     */
+    println("Duplicate me using ⌘ + D shortcut")
 }
