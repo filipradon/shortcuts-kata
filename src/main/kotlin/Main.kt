501465ad0024fa01a36311e6fba84757705fca17
Filip Radoń <filip.radon@ocado.com>
2022-08-11 13:25:50 +0200

    Third shortcut - Search Everywhere

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,11 @@ fun main(args: Array<String>) {
     // ⌘ + D - Duplicate line
     println("Duplicate me using ⌘ + D shortcut")
 
-
     // ⌘ + / - Comment/Uncomment line
     println("Comment me using ⌘ + / short")
 //    println("Uncomment me using ⌘ + / short")
+
+    // Double ⇧ - Search Everywhere
+    println("Press ⇧ two times to open new Search Everywhere window")
 
 }
