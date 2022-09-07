bdc4037318a2f8811be019dad2b314d7e7ebc428
Filip Radoń <filip.radon@ocado.com>
2022-09-07 21:10:38 +0200

    Multicursor/Column selection shortcuts pt I

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,9 @@ fun main(args: Array<String>) {
     // ⌘ + D - Duplicate line
     println("Duplicate me using ⌘ + D shortcut")
 
+    // ⌘ + Backspace - Delete line
+    println("Delete the line using ⌘ + Backspace")
+
     // ⌘ + / - Comment/Uncomment line
     println("Comment me using ⌘ + / short")
 //    println("Uncomment me using ⌘ + / short")
@@ -11,4 +14,18 @@ fun main(args: Array<String>) {
     // Double ⇧ - Search Everywhere
     println("Press ⇧ two times to open new Search Everywhere window")
 
+
+    // Source: https://www.vojtechruzicka.com/intellij-idea-tips-tricks-multiple-cursors/#:~:text=When%20holding%20Alt%20%2B%20Shift%20(or,need%20to%20be%20vertically%20aligned.
+    // Select all the lines and then have multi cursor at the beginning of each
+    // Use ⇧ + down arrow to select lines
+    // Use ⇧ + ⌘ + 8 to have multicursor
+    val a = "First"
+    val b = "Second"
+    val c = "Third"
+
+    //
+    // Press ⌥ once then press it again and hold and by using ↑ and ↓ clone cursor selecting all the lines
+    val d = "First"
+    val e = "Second"
+    val f = "Third"
 }
